4a15adce2df40598e083701daae8d4eec4f224f1
SungWon Jung <s26059@gsm.hs.kr>
2026-07-02 13:52:12 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/cocky/cockyserver/CockyServerApplication.java
@@ -0,0 +1,13 @@
+package com.cocky.cockyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CockyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cocky/cockyserver/CockyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocky.cockyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
